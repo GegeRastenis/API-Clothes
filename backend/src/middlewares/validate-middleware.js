bb3bf2a0981955189372509f88383385189807bc
kprieto <anakarenprieto@live.com>
2025-06-24 15:31:58 -0700

    Actualizacion de Middlewares

---
+++ backend/src/middlewares/validate-middleware.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+export const validateMiddleware = (req, res, next) => {
+    const { product, size } = req.body;
+
+    if (!product || typeof product !== 'string') {
+        res.status(400).json({ error: "El campo 'product' es requerido y debe ser string" });
+        return;
+    }
+
+    if (!size || typeof size !== 'string') {
+        res.status(400).json({ error: "El campo 'size' es requerido y debe ser string" });
+        return;
+    }
+
+    next();
+};
