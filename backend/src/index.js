3b879c6a042db71a47309f5a9d6e0a05786bb269
Fiorella <fiorellaroma98@gmail.com>
2025-06-26 00:13:46 -0700

    Cambiando css de inicio

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,6 +22,9 @@ app.use('/api/users', usersRouter)
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../../public/index.html'))
 })
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/inicio.html'))
+})
 
 app.listen(PORT, ()=>{
 
