c6bd225ef8bd4140303fa9027f6dc33753e86d03
Fiorella <fiorellaroma98@gmail.com>
2025-06-26 11:26:12 -0700

    Cambiando estructura de un producto y de sus propiedades

--- backend/src/middlewares/validate-middleware.js
+++ backend/src/middlewares/validate-middleware.js
@@ -1,16 +1,24 @@
 const express = require('express');
 
 const validateMiddleware = (req, res, next) => {
-    const { product, size } = req.body;
+    const { name, size, price, image } = req.body;
 
-    if (!product || typeof product !== 'string') {
-        res.status(400).json({ error: "El campo 'product' es requerido y debe ser string" });
+    if (!name || typeof name !== 'string') {
+        res.status(400).json({ error: "El campo 'name' es requerido y debe ser string" });
         return;
     }
 
     if (!size || typeof size !== 'string') {
         res.status(400).json({ error: "El campo 'size' es requerido y debe ser string" });
         return;
+    }
+
+    if(!price || price === undefined || typeof price != 'number'){
+        return res.status(400).json({error: "El campo 'price' es requerido y deber ser un numero"})
+    }
+    
+    if(!image || typeof image != 'string'){
+        return res.status(400).json({error: "El campo 'image' debe ser un string"})
     }
 
     next();
