58954cd76bf1eccbf61f6c247045f586f2831116
Fiorella <fiorellaroma98@gmail.com>
2025-06-29 00:56:40 -0700

    terminando paginas

--- public/admin.js
+++ public/admin.js
@@ -13,15 +13,21 @@ document.addEventListener('DOMContentLoaded', async() => {
       productsContainer.innerHTML = ''; //Se limpia antes de agregar
       products.forEach(product => {
         const productDiv = document.createElement('div');
-        productDiv.className = 'product';
+        productDiv.className = 'product-card';
         productDiv.innerHTML = `
-          <img src="${product.image || './images/default-product.png'}" alt="${product.name}">
-          <h4>${product.name}</h4>
-          <p>$${product.price.toFixed(2)}</p>
-          <p>${product.size}</p>
-          <div class="product-actions"> 
-            <button class="edit-btn" data-id="${product.id} title="Editar">✏️</button>
-            <button class="delete-btn" data-id=${product.id} title="Eliminar">🗑️ </button>
+          <div class="product-card-img">
+            <img src="${product.image || './images/default-product.png'}" alt="${product.name}">
+            <div class="product-icons">
+              <button class="edit-btn" data-id="${product.id}" title="Editar">✏️</button>
+              <button class="delete-btn" data-id="${product.id}" title="Eliminar">🗑️</button>
+            </div>
+          </div>
+          <div class="product-card-info">
+            <h4 class="product-name">${product.name}</h4>
+            <p class="product-price">
+              <span class="price-current">${product.price.toFixed(2)} </span>
+            </p>
+            <p class="product-size">Talla:${product.size}</p>
           </div>
         `;
         productsContainer.appendChild(productDiv);
@@ -29,127 +35,126 @@ document.addEventListener('DOMContentLoaded', async() => {
       //Evento para editar producto
       document.querySelectorAll('.edit-btn').forEach(button => {
         button.addEventListener('click', async(e) => {
-            const productId = e.target.dataset.id;
+          const productId = e.target.dataset.id;
 
-            try{
-                const res = await fetch(`/api/clothes/${productId}`);
-                if(!res.ok) alert('No se pudo obtener el producto');
-                const product = await res.json();
-                document.getElementById('edit-form').style.display = 'flex';
-                document.getElementById('edit-id').value = product.id;
-                document.getElementById('edit-name').value = product.name;
-                document.getElementById('edit-price').value = product.price;
-                document.getElementById('edit-size').value = product.size;
-                document.getElementById('edit-image').value = product.image || '';
-
-            } catch(error){
-                alert('Error al cargar el producto');
-                console.error(error);
-            }
+          try{
+            const res = await fetch(`/api/clothes/${productId}`);
+            if(!res.ok) alert('No se pudo obtener el producto');
+            const product = await res.json();
+            document.getElementById('edit-form').style.display = 'flex';
+            document.getElementById('edit-id').value = product.id;
+            document.getElementById('edit-name').value = product.name;
+            document.getElementById('edit-price').value = product.price;
+            document.getElementById('edit-size').value = product.size;
+            document.getElementById('edit-image').value = product.image || '';
+          } catch(error){
+            alert('Error al cargar el producto');
+            console.error(error);
+          }
         })
       })
 
-    document.getElementById('edit-form').addEventListener('submit', async(e) => {
+      document.getElementById('edit-form').addEventListener('submit', async(e) => {
         e.preventDefault();
         const productId = document.getElementById('edit-id').value;
         const updatedProduct = {
-            name: document.getElementById('edit-name').value,
-            price: parseFloat(document.getElementById('edit-price').value),
-            size: document.getElementById('edit-size').value,
-            image: document.getElementById('edit-image').value
+          name: document.getElementById('edit-name').value,
+          price: parseFloat(document.getElementById('edit-price').value),
+          size: document.getElementById('edit-size').value,
+          image: document.getElementById('edit-image').value
         };
 
         try {
-            const res = await fetch(`/api/clothes/${productId}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(updatedProduct)
-            });
+          const res = await fetch(`/api/clothes/${productId}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(updatedProduct)
+          });
 
-            if (res.ok) {
-                location.reload();
-            } else {
-                alert('Error al actualizar el producto');
-            }
-        } catch (error) {
-            console.error(error);
-            alert('Error al conectar con el servidor');
+          if (res.ok) {
+              location.reload();
+          } else {
+              alert('Error al actualizar el producto');
+          }
+        }catch (error) {
+          console.error(error);
+          alert('Error al conectar con el servidor');
         }
-    });
-
-          //Evento para editar producto
-    document.querySelectorAll('.add-btn').forEach(button => {
-      button.addEventListener('click', () => {
-        const form = document.getElementById('add-form');
-        form.style.display = 'flex'; // Mostrar el formulario
-        form.reset(); // Limpiar campos del formulario (requiere <form id="add-form">)
-
-        // También puedes limpiar los campos manualmente si no usas <form>
-        document.getElementById('add-name').value = '';
-        document.getElementById('add-description').value = '';
-        document.getElementById('add-price').value = '';
-        document.getElementById('add-size').value = '';
-        document.getElementById('add-image').value = '';
       });
-    });
 
-        document.getElementById('add-form').addEventListener('submit', async(e) => {
+      //Evento para editar producto
+      document.querySelectorAll('.add-btn').forEach(button => {
+        button.addEventListener('click', () => {
+          const form = document.getElementById('add-form');
+          form.style.display = 'flex'; // Mostrar el formulario
+          form.reset(); // Limpiar campos del formulario (requiere <form id="add-form">)
+
+          // También puedes limpiar los campos manualmente si no usas <form>
+          document.getElementById('add-name').value = '';
+          document.getElementById('add-description').value = '';
+          document.getElementById('add-price').value = '';
+          document.getElementById('add-size').value = '';
+          document.getElementById('add-image').value = '';
+        });
+      });
+
+      //Evento para agregar un producto
+      document.getElementById('add-form').addEventListener('submit', async(e) => {
         e.preventDefault();
-        const productId = document.getElementById('edit-id').value;
         const createProduct = {
-            name: document.getElementById('add-name').value,
-            description: document.getElementById('add-description'),
-            price: parseFloat(document.getElementById('add-price').value),
-            size: document.getElementById('add-size').value,
-            image: document.getElementById('add-image').value
+          name: document.getElementById('add-name').value,
+          description: document.getElementById('add-description').value,
+          price: parseFloat(document.getElementById('add-price').value),
+          size: document.getElementById('add-size').value,
+          image: document.getElementById('add-image').value
         };
 
-        try {
-            const res = await fetch(`/api/clothes/`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(createProduct)
-            });
+      try {
+        const res = await fetch(`/api/clothes/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(createProduct)
+        });
 
-            const result = await res.json();
+        const result = await res.json();
 
-            if (res.ok) {
-                location.reload();
-                alert('Producto registrado exitosamente ✔️');
-                document.getElementById('add-form').reset();
-                document.getElementById('add-form').style.display = 'none';
-            } else {
-                alert(`Error: ${result.error}`);
-            }
-        } catch (error) {
-            console.error(error);
-            alert('Error al registrar el producto');
+        if (res.ok) {
+          location.reload();
+          alert('Producto registrado exitosamente ✔️');
+          document.getElementById('add-form').reset();
+          document.getElementById('add-form').style.display = 'none';
+        } else {
+          alert(`Error: ${result.error}`);
         }
+      }catch (error) {
+        console.error(error);
+        alert('Error al registrar el producto');
+      }
     });
 
-    document.querySelectorAll('.delete-btn').forEach(button => {
+      document.querySelectorAll('.delete-btn').forEach(button => {
         button.addEventListener('click', async(e) => {
-            const idProduct = e.target.dataset.id;
-            if(confirm("Seguro de eliminar el producto?")){
-                try{
-                    const res = await fetch(`/api/clothes/${idProduct}`, {
-                        method: 'DELETE'
-                    });
-    
-                    if(res.ok){
-                        location.reload(); //Recarga los productos
-                    } else {
-                        alert('No se pudo eliminar el producto, hubo un error');
-                    }
-                } catch(error){
-                    console.error(err);
-                    alert('Error al conectar con el servidor');
-                }
+          const idProduct = e.target.dataset.id;
+          if(confirm("Seguro de eliminar el producto?")){
+            try{
+              const res = await fetch(`/api/clothes/${idProduct}`, {
+                  method: 'DELETE'
+              });
+
+              if(res.ok){
+                  location.reload(); //Recarga los productos
+              } else {
+                  alert('No se pudo eliminar el producto, hubo un error');
+              }
+            } catch(error){
+              console.error(err);
+              alert('Error al conectar con el servidor');
             }
+          }
         })
       })
     } else {
-        productsContainer.innerHTML = `<p>Error al cargar productos: ${products.error}</p>`;
+      productsContainer.innerHTML = `<p>Error al cargar productos: ${products.error}</p>`;
     }
   } catch(error){
     console.error('Error al cargar productos:', error);
---
+++ public/shop.js
@@ -0,0 +1,32 @@
+//Muestra los productos obteniendolos de la api, esto se hace desde que carga la pagina principal
+document.addEventListener('DOMContentLoaded', async() => {
+  const productsContainer = document.getElementById('products-list');
+  try{
+    const res = await fetch('/api/clothes', {
+      headers: {
+        'Content-Type' : 'application/json',
+      }
+    })
+    const products = await res.json();
+
+    if(res.ok){
+      productsContainer.innerHTML = ''; //Se limpia antes de agregar
+      products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.className = 'product';
+        productDiv.innerHTML = `
+          <img src="${product.image || './images/default-product.png'}" alt="${product.name}">
+          <h4>${product.name}</h4>
+          <p>$${product.price.toFixed(2)}</p>
+          <p>${product.description}</p>
+        `;
+        productsContainer.appendChild(productDiv);
+      })
+    } else {
+      productsContainer.innerHTML = `<p>Error al cargar productos: ${products.error}</p>`;
+    }
+  }catch(error){
+    console.error('Error al cargar productos:', error);
+    productsContainer.innerHTML = `<p>No se pudieron cargar los productos.</p>`;
+  }
+});
